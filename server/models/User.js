719f5f1ebbd52b94807dfffa467f3139c7c60f03
akram ahil <akram.ahil123@gmail.com>
2025-05-26 10:29:43 +0100

    Initial implementation of the project structure and basic functionality.

---
+++ server/models/User.js
@@ -0,0 +1,48 @@
+const {DataTypes} = require("sequelize");
+
+module.exports = (sequelize) => {
+    const User = sequelize.define('User', {
+        id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        },
+        firstName: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        },
+        lastName: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        },
+        password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        },
+        role: {
+        type: DataTypes.ENUM('Admin', 'School Administrator', 'Guard', 'Teacher', 'Student', 'Parent'),
+        allowNull: false,
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: true, 
+            unique: true,
+            validate: {
+                isEmail: true,
+            }
+        },
+        phone: {
+          type: DataTypes.STRING,
+        },
+        address:{
+           type: DataTypes.STRING,  
+        }
+     
+    }, {
+   
+      timestamps: true,
+    });
+  
+    return User;
+  };
+  
