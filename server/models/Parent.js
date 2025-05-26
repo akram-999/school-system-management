77d3e68f807d0ba65d58f23da4c9c01740d54f88
akram ahil <akram.ahil123@gmail.com>
2025-05-26 10:50:51 +0100

    Enhance User model by further optimizing data structure and removing additional redundant fields

---
+++ server/models/Parent.js
@@ -0,0 +1,37 @@
+const { DataTypes } = require("sequelize");
+
+module.exports = (sequelize) => {
+  const Parent = sequelize.define("Parent", {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'Users',
+        key: 'id',
+      }
+    },
+    phoneNumber: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
+    address: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    }
+  }, {
+    timestamps: true,
+  });
+
+//   Parent.associate = (models) => {
+//     Parent.belongsTo(models.User, { foreignKey: 'userId' });
+//     // Many-to-many association with Students
+//     Parent.belongsToMany(models.Student, { through: 'StudentParent' });
+//   };
+
+  return Parent;
+};
